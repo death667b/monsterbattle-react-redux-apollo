17bce6372d5cead5275c353ad0a6f68ad0aa5b16
death667b <idaniel@spinnys.com>
2017-09-16 14:55:20 +1000

    enabled redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import GameBoard from './components/GameBoard'
 
-import withLocalStateLogic from './containers/withLocalStateLogic'
-// import withReduxLogic from './containers/withReduxLogic'
+// import withLocalStateLogic from './containers/withLocalStateLogic'
+import withReduxLogic from './containers/withReduxLogic'
 // import withApolloLogic from './containers/withApolloLogic'
 
-const App = withLocalStateLogic(GameBoard)
+// const App = withLocalStateLogic(GameBoard)
 // const App = withReduxLogic(GameBoard)
-// const App = withApolloLogic(GameBoard)
+const App = withApolloLogic(GameBoard)
 
 export default App
