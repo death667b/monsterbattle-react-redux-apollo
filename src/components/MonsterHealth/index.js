91a47bdb036dcc4335affb5d9695ed4a0397530a
death667b <idaniel@spinnys.com>
2017-09-16 11:32:44 +1000

    Layout change to MonsterHealth

--- src/components/MonsterHealth/index.js
+++ src/components/MonsterHealth/index.js
@@ -9,9 +9,10 @@ const MonsterHealth = ({
     flip
 }) =>
     <div className={cx('MonsterHealth', {'MonsterHealth--flip': flip})}>
-        <div className={'MonsterHealth__bar'} style={{
-            width: (Math.min(currentHealth / health, 1) / 100) + '%'
-        }}/>
+        <div 
+            className={'MonsterHealth__bar'} 
+            style={{ width: (Math.min(currentHealth / health, 1) / 100) + '%'}}
+        />
     </div>
 
 export default MonsterHealth
