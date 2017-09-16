389e083caf1ce0d9a925c87dea328fde8dcc7b74
death667b <idaniel@spinnys.com>
2017-09-16 11:39:37 +1000

    Accept MonsterName as child in MonsterHealth

--- src/components/MonsterHealth/index.js
+++ src/components/MonsterHealth/index.js
@@ -6,13 +6,17 @@ import './styles.css';
 const MonsterHealth = ({
     health,
     currentHealth,
-    flip
+    flip,
+    children
 }) =>
     <div className={cx('MonsterHealth', {'MonsterHealth--flip': flip})}>
         <div 
             className={'MonsterHealth__bar'} 
-            style={{ width: (Math.min(currentHealth / health, 1) / 100) + '%'}}
+            style={{ width: Math.min(currentHealth / health, 1) * 100 + '%'}}
         />
+        {children && <div className={'MonsterHealth__info'}>
+            {children}
+        </div>}
     </div>
 
 export default MonsterHealth
