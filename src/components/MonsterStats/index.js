34bf617a775e8db0272303ae957607dfea1a0a3e
death667b <idaniel@spinnys.com>
2017-09-16 11:47:51 +1000

    Start adding MonsterStats

--- src/components/MonsterStats/index.js
+++ src/components/MonsterStats/index.js
@@ -1,1 +1,14 @@
-export default () => false
+import React from 'react';
+
+import './styles.css';
+
+const MonsterStats = ({
+    attack,
+    defense,
+    health
+}) => 
+    <div>
+        
+    </div>
+
+export default MonsterStats
