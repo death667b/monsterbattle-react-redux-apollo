85c3436eb526f05f49b768c84fa6850438c10d3c
death667b <idaniel@spinnys.com>
2017-09-16 11:26:52 +1000

    Adding width styles to MonsterBattleView

--- src/components/MonsterHealth/index.js
+++ src/components/MonsterHealth/index.js
@@ -1,1 +1,17 @@
-export default () => false
+import React from 'react';
+import cx from 'classnames';
+
+import './styles.css';
+
+const MonsterHealth = ({
+    health,
+    currentHealth,
+    flip
+}) =>
+    <div className={cx('MonsterHealth', {'MonsterHealth--flip': flip})}>
+        <div className={'MonsterHealth__bar'} style={{
+            width: currentHealth / health
+        }}/>
+    </div>
+
+export default MonsterHealth
